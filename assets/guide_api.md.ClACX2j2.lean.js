b44d8a6405005d2f7f20f9e28c0a2f04ebd54a0d
Mattis44 <Mattis44@users.noreply.github.com>
2026-01-09 20:46:52 +0000

    deploy: 650ce300b2b1ea91a2c13f6fd807ddceebd083c1

---
+++ assets/guide_api.md.ClACX2j2.lean.js
@@ -0,0 +1,1 @@
+import{_ as i,c as a,o as t,aj as e}from"./chunks/framework.BIf7xRIw.js";const E=JSON.parse('{"title":"API","description":"","frontmatter":{},"headers":[],"relativePath":"guide/api.md","filePath":"guide/api.md"}'),n={name:"guide/api.md"};function l(h,s,p,k,d,r){return t(),a("div",null,[...s[0]||(s[0]=[e("",12)])])}const c=i(n,[["render",l]]);export{E as __pageData,c as default};
