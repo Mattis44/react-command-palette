72273d88440232464aecac159fb22db25d35a59c
Mattis44 <Mattis44@users.noreply.github.com>
2026-01-23 16:56:17 +0000

    deploy: 07a347732e050453de37da842dfe6efdd445a413

---
+++ assets/guide_api.md.C2d1_a_m.lean.js
@@ -0,0 +1,1 @@
+import{_ as i,o as a,c as t,aj as e}from"./chunks/framework.D8-Y3fGF.js";const E=JSON.parse('{"title":"API","description":"","frontmatter":{},"headers":[],"relativePath":"guide/api.md","filePath":"guide/api.md"}'),n={name:"guide/api.md"};function l(h,s,p,k,d,r){return a(),t("div",null,[...s[0]||(s[0]=[e("",12)])])}const c=i(n,[["render",l]]);export{E as __pageData,c as default};
